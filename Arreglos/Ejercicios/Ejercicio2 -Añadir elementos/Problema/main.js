e58e0b9d56889cee7251de53dd112b82210caf20
ValeriaPerez <valeria.pjaimes@gmail.com>
2016-10-03 17:04:35 -0500

    Agrego carpeto Arreglos con capturas y ejercicios

---
+++ Arreglos/Ejercicios/Ejercicio2 -Añadir elementos/Problema/main.js
@@ -0,0 +1,10 @@
+// Escribe aquí tu codigo Javascriptfor	
+var nombres = ["Valeria", "Bere", "Claudia"];
+	
+	for (var i = 0; i <= 6; i++) {
+	
+		nombres.push(prompt("Escribe un nombre pues..."));			
+}
+document.write(nombres);
+
+
